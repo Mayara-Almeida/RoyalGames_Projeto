8eecffb3212df5ddd03b35599a413e6880d9fb68
Mayara Almeida <mayara.s.almeida8@aluno.senai.br>
2026-05-18 12:19:12 -0300

    :sparkles: Ajustes visuais na tela login

--- royalgames/src/components/botao/botao.tsx
+++ royalgames/src/components/botao/botao.tsx
@@ -4,13 +4,14 @@ import styles from "./botao.module.css"
 type PropsBotao = { // Props -> são a informações/dados que são enviadas para o componente (nesse caso o botão) e guardadas 
     children: ReactNode; // Componente vai aceitar conteúdo dentro dele(que pode mudar) e guardar, ex: o texto do botão 
     onClick?: () => void ; // Guardar uma função para o evento de clique, mas pode ser opcional, portanto não retorna nada(void)
+    className?: string // O componente botão aceita classes extras, sendo opcional
 }
 
-const Botao = ({children, onClick}: PropsBotao) => { // Cria o componente que recebe como parâmetro as props(informações) do PropsBotao e guarda
+const Botao = ({children, onClick, className}: PropsBotao) => { // Cria o componente que recebe como parâmetro as props(informações) do PropsBotao e guarda
     return (
-        <button onClick={onClick} className={styles.botao} type="submit"> {/* onClick={onclick} -> Passa uma função q é executada ao clicar */}
+        <button onClick={onClick} className={`${styles.botao} ${className || ""}`} type="submit"> {/* onClick={onclick} -> Passa uma função q é executada ao clicar */}
             {children} {/* Botão vai ter o conteúdo(texto) que veio das props para mostrar na tela */}
         </button>
-    )
-}
+        )
+    }
 export default Botao;
--- royalgames/src/pages/login/index.tsx
+++ royalgames/src/pages/login/index.tsx
@@ -8,18 +8,18 @@ const Login = () => {
                 <div className={styles.imagem_mulher}>
                     <img src="./imgs/mulher_login.svg" alt="Mulher com visual futurista cyberpunk, usando acessório tecnológico neon nos olhos e tranças claras." />
                 </div>
-                    <div className={`efeito_vidro ${styles.form_login}`}>
-                        <img  className={styles.logo} src="./imgs/logo.svg" alt="Logo do Royal Games em tom rosa destaque." />
-                        <form id={styles.formulario} action="" >
+                    <div className={`efeito_vidro_card ${styles.form_login}`}>
+                        <img className={styles.logo} src="./imgs/logo.svg" alt="Logo do Royal Games em tom rosa destaque." />
+                        <form className={styles.formulario} action="" >
                             <div className={styles.campo_form}>
                                 <label htmlFor="email">Email</label>
-                                <input type="text" name="email" required />
+                                <input className="efeito_vidro_input" type="text" name="email" required /> {/* importando classe da global, por isso não coloca o styles. */}
                             </div>
                             <div className={styles.campo_form}>
                                 <label htmlFor="senha">Senha</label>
-                                <input type="password" name="senha" required />
+                                <input className="efeito_vidro_input" type="password" name="senha" required />
                             </div>
-                            <Botao>Entrar</Botao>
+                            <Botao className={styles.botao_entrar}>Entrar</Botao>
                         </form>
                     </div>
             </main>
